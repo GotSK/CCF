ba63ec951a40902894f3f79c5ae27e5c7d9a111e
Christian <serious.spam@yahoo.de>
2015-01-21 03:31:22 +0100

    various additions

--- Keymulator/src/Database.py
+++ Keymulator/src/Database.py
@@ -11,6 +11,17 @@ class UserAlreadyExistsError(Exception):
         return str(self.msg)
     
     __repr__ = __str__
+    
+class NoSuchUserError(Exception):
+
+    def __init__(self, username):
+        self.username = username
+        self.msg = 'A user with name ' + username + ' does not exist.'
+        
+    def __str__(self):
+        return str(self.msg)
+    
+    __repr__ = __str__
 
 class User():
     
@@ -57,6 +68,15 @@ class Database():
         self.users.append(newUser)
         self.userByName[username] = newUser
     
+    def hasUser(self, username):
+        return username in self.userByName.keys()
+        
+    def getInfluenceByName(self, username):
+        return self.__getUserByName__(username).influence
+    
+    def getReputationByName(self, username):
+        return self.__getUserByName__(username).reputation
+    
     def modifyUserRep(self, username, mod):
         self.__getUserByName__(username).modifyReputation(mod)
         
@@ -86,6 +106,11 @@ class Database():
             self.setUserInf(name, val)
     
     def __getUserByName__(self, username):
+        try:
+            name = self.userByName[username]
+        except KeyError:
+            raise NoSuchUserError(username)
+        
         return self.userByName[username]
     
         
